acbb6ac25aa27c1f0becccf89805fe2b52b7d776
DATDT-TT-D1\BadassPsycho <qwerty.lynkaa@gmail.com>
2018-07-24 19:55:00 +0700

    - Add Table detail. - Custom item for Admin Activity, Table. - Template item for Order food.

--- app/src/main/java/com/example/tungpham/orderfood/ui/activity/CookerActivity.java
+++ app/src/main/java/com/example/tungpham/orderfood/ui/activity/CookerActivity.java
@@ -1,7 +1,11 @@
 package com.example.tungpham.orderfood.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.tungpham.orderfood.R;
 
 public class CookerActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class CookerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooker);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_profile) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
